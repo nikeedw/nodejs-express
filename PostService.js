a980d9f6f7e40b477cbfd2b78aa87ff342319d5e
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-02-13 20:25:10 +0200

    Added data acces layer from PostService

---
+++ PostService.js
@@ -0,0 +1,39 @@
+import Post from "./Post.js";
+
+class PostService {
+	async create(post) {
+		const createdPost = await Post.create(post);
+		return createdPost;
+	}
+
+	async getAll() {
+		const posts = await Post.find();
+		return posts;
+	}
+	async getOne(id) {
+		if (!id) {
+			throw new Error('не указан ID')
+		}
+		const post = await Post.findById(id);
+		return post;
+	}
+
+	async update(post) {
+		if (!post._id) {
+			throw new Error('не указан ID')
+		}
+		const updatedPost = await Post.findByIdAndUpdate(post._id, post, { new: true })
+		return updatedPost;
+	}
+
+	async delete(id) {
+		if (!id) {
+			throw new Error('не указан ID')
+		}
+		const post = await Post.findByIdAndDelete(id);
+		return post;
+	}
+}
+
+
+export default new PostService();
